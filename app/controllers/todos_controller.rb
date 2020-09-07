89e8ecdd3357cf92506bfaec72b67f84550f57f7
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-09-07 18:12:12 -0300

    Função para salvar uma tarefa

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,11 @@
 class TodosController < ApplicationController
   def new
   end
+  
+  def create
+    @todo = Todo.new params.require(:todo).permit :description, :status
+
+    @todo.save
+    redirect_to @todo
+  end
 end
